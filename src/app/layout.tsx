c62195f98b615e047bd2c53ebf2dd2f34d08862e
mateus-werneck <werneck.mateus@gmail.com>
2023-04-09 10:25:44 -0300

    Ajustes Login

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,76 +14,76 @@ export const metadata = {
   icons: [
     {
       rel: 'shortcut-icon',
-      url: '/favicon.ico'
+      url: './favicon.ico'
     },
     {
       rel: 'apple-touch-icon',
       sizes: '57x57',
-      url: '/apple-icon-57x57.png'
+      url: './apple-icon-57x57.png'
     },
     {
       rel: 'apple-touch-icon',
       sizes: '60x60',
-      url: '/apple-icon-60x60.png'
+      url: './apple-icon-60x60.png'
     },
     {
       rel: 'apple-touch-icon',
       sizes: '72x72',
-      url: '/apple-icon-72x72.png'
+      url: './apple-icon-72x72.png'
     },
     {
       rel: 'apple-touch-icon',
       sizes: '76x76',
-      url: '/apple-icon-76x76.png'
+      url: './apple-icon-76x76.png'
     },
     {
       rel: 'apple-touch-icon',
       sizes: '114x144',
-      url: '/apple-icon-114x144.png'
+      url: './apple-icon-114x144.png'
     },
     {
       rel: 'apple-touch-icon',
       sizes: '120x120',
-      url: '/apple-icon-120x120.png'
+      url: './apple-icon-120x120.png'
     },
     {
       rel: 'apple-touch-icon',
       sizes: '144x144',
-      url: '/apple-icon-144x144.png'
+      url: './apple-icon-144x144.png'
     },
     {
       rel: 'apple-touch-icon',
       sizes: '152x152',
-      url: '/apple-icon-152x152.png'
+      url: './apple-icon-152x152.png'
     },
     {
       rel: 'apple-touch-icon',
       sizes: '180x180',
-      url: '/apple-icon-180x180.png'
+      url: './apple-icon-180x180.png'
     },
     {
       rel: 'icon',
       type: 'image/png',
       sizes: '192x192',
-      url: '/android-icon-192x192.png"'
+      url: './android-icon-192x192.png"'
     },
     {
       rel: 'icon',
       type: 'image/png',
       sizes: '32x32',
-      url: '/favicon-32x32.png"'
+      url: './favicon-32x32.png"'
     },
     {
       rel: 'icon',
       type: 'image/png',
       sizes: '96x96',
-      url: '/favicon-96x96.png"'
+      url: './favicon-96x96.png"'
     },
     {
       rel: 'icon',
       type: 'image/png',
       sizes: '16x16',
-      url: '/favicon-16x16.png"'
+      url: './favicon-16x16.png"'
     }
   ]
 };
--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,7 +1,17 @@
 import { LoginForm } from '@Components/Form/Login';
+import { useAuth } from '@Contexts/AuthContext';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 import { FormModal, LoginContainer } from './style';
 
 export function LoginPage() {
+  const { hasSession } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (hasSession()) router.push('/');
+  }, []);
+
   return (
     <>
       <LoginContainer>
