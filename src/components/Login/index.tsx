1778e4e3551d06bc2a1976b9ef0661dc1774d725
mateus-werneck <werneck.mateus@gmail.com>
2023-05-01 09:20:06 -0300

    Ajustes Login

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,12 +1,21 @@
+'use client';
 import { LoginForm } from '@Components/Form/Login';
+import { useAuth } from '@Contexts/AuthContext';
+import { redirect } from 'next/navigation';
 import { FormModal, LoginContainer } from './style';
 
 export function LoginPage() {
+  const { hasSession } = useAuth();
+
+  if (hasSession()) {
+    redirect('/');
+  }
+
   return (
-      <LoginContainer>
-        <FormModal>
-          <LoginForm />
-        </FormModal>
-      </LoginContainer>
+    <LoginContainer>
+      <FormModal>
+        <LoginForm />
+      </FormModal>
+    </LoginContainer>
   );
 }
